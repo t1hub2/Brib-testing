264946e2fd3ecdad69b8cf86557fd64ee0410d02
HELORER <scriptingrobloxinger@gmail.com>
2024-12-02 13:33:10 +0700

    Update Testing code.lua

--- .github/workflows/Testing code.lua
+++ .github/workflows/Testing code.lua
@@ -144,3 +144,4 @@ local Button = Tab:CreateButton({
   Callback = function()
     loadstring(game:HttpGet("https://gist.githubusercontent.com/T0mix1608/ff0a607a510996f361c230a2de85f9e8/raw/7096f3796a8feb208b6c0b9029686150e878b7a1/gistfile1.txt", true))() 
     end    
+   })

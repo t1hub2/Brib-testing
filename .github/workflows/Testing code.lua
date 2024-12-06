61638d2e781ddeb9cc86c482b8c64adedb843ae7
t1hub2 <solavolodimir@gmail.com>
2024-12-06 06:59:14 +0100

    Update Testing code.lua

--- .github/workflows/Testing code.lua
+++ .github/workflows/Testing code.lua
@@ -1,3 +1,14 @@
+---====== Load achievement giver ======---
+local achievementGiver = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors/Custom%20Achievements/Source.lua"))()
+
+---====== Display achievement ======---
+achievementGiver({
+    Title = "Приветик!",
+    Desc = "Это T1 hub!",
+    Reason = "Запустить T1 hub",
+    Image = "129826331604126"
+})
+
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 local Window = Rayfield:CreateWindow({
    Name = "T1 hub / Первый русский скрипт",

cb367967d9a32ea66bc1e631cb9e07d54b1a7e62
t1hub2 <solavolodimir@gmail.com>
2024-12-06 06:21:24 +0100

    Update Testing code.lua

--- .github/workflows/Testing code.lua
+++ .github/workflows/Testing code.lua
@@ -144,3 +144,9 @@ local Button = Tab:CreateButton({
     loadstring(game:HttpGet("https://gist.githubusercontent.com/T0mix1608/ff0a607a510996f361c230a2de85f9e8/raw/7096f3796a8feb208b6c0b9029686150e878b7a1/gistfile1.txt", true))() 
     end    
    })
+local Button = Tab:CreateButton({
+   Name = "Графика 2017 года",
+   Callback = function()
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/Idk-lol2/2017mode/refs/heads/main/2017MODEtxt.txt"))()-- The function that takes place when the button is pressed
+   end,
+})
